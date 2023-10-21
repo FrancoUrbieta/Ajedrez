6d0811c866be0781fa4e2ad83f5bed49dbfadac9
FrancoUrbieta <148584575+FrancoUrbieta@users.noreply.github.com>
2023-10-20 21:56:03 -0300

    se agrego piezas en el tablero

--- Source.cpp
+++ Source.cpp
@@ -6,18 +6,28 @@
 class Tablero
 {
     public:
-        sf::Sprite m_casillaB[8];
-        sf::Sprite m_casillaN[8];
+        sf::Sprite m_casillaI[8];
+        sf::Sprite m_casillaJ[8];
         int m_posNum[8];
         char m_posLtr[8];
         bool m_ocp = false;
-        Tablero(sf::Image imagenN, sf::Image imagenB) {}
+        Tablero() {}
+};
+
+class Pieza
+{
+    public:
+        sf::Sprite m_imagen;
+        int m_posNum[8];
+        char m_posLtr[8];
 };
 
 void Window()
 {
     sf::RenderWindow tablero(sf::VideoMode(900, 600), "Tablero");
     sf::Texture casillaB, casillaN;
+    sf::Texture peonB, peonN, reyB, reyN, damaB, damaN;
+    sf::Texture torreB, torreN, caballoB, caballoN, alfilB, alfilN;
 
     casillaB.loadFromFile("CasillaB.PNG");
     casillaN.loadFromFile("CasillaN.PNG");
@@ -30,6 +40,42 @@ void Window()
 
     spriteB.setOrigin(-40, -20);
     spriteN.setOrigin(-40, -20);
+
+    peonB.loadFromFile("PeonB.PNG");
+    peonN.loadFromFile("PeonN.PNG");
+
+    sf::Sprite spritePeonB(peonB);
+    sf::Sprite spritePeonN(peonN);
+
+    spritePeonB.setScale(2, 2);
+    spritePeonN.setScale(2, 2);
+
+    spritePeonB.setOrigin(-40, -20);
+    spritePeonN.setOrigin(-40, -20);
+
+    torreB.loadFromFile("TorreB.PNG");
+    torreN.loadFromFile("TorreN.PNG");
+
+    sf::Sprite spriteTorreB(torreB);
+    sf::Sprite spriteTorreN(torreN);
+
+    spriteTorreB.setScale(2, 2);
+    spriteTorreN.setScale(2, 2);
+
+    spriteTorreB.setOrigin(-40, -20);
+    spriteTorreN.setOrigin(-40, -20);
+
+    alfilB.loadFromFile("AlfilB.PNG");
+    alfilN.loadFromFile("AlfilN.PNG");
+
+    sf::Sprite spriteAlfilB(alfilB);
+    sf::Sprite spriteAlfilN(alfilN);
+
+    spriteAlfilB.setScale(2, 2);
+    spriteAlfilN.setScale(2, 2);
+
+    spriteAlfilB.setOrigin(-40, -20);
+    spriteAlfilN.setOrigin(-40, -20);
 
     while (tablero.isOpen())
     {
@@ -59,6 +105,44 @@ void Window()
                 {
                     tablero.draw(spriteN);
                     spriteN.setPosition(sf::Vector2f(distancia * i, distancia * j));
+                }
+            }
+        }
+
+        for (int i = 0; i < 8; i++)
+        {
+            float distancia = 64;
+            for (int j = 0; j < 8; j++)
+            {
+                if (j == 1)
+                {
+                    tablero.draw(spritePeonN);
+                    spritePeonN.setPosition(sf::Vector2f(distancia * i, distancia * j));
+                }
+                if (j == 6)
+                {
+                    tablero.draw(spritePeonB);
+                    spritePeonB.setPosition(sf::Vector2f(distancia * i, distancia * j));
+                }
+                if ((i == 2 && j == 0) || (i == 5 && j == 0))
+                {
+                    tablero.draw(spriteAlfilN);
+                    spriteAlfilN.setPosition(sf::Vector2f(distancia * i, distancia * j));
+                }
+                if ((i == 2 && j == 7) || (i == 5 && j == 7))
+                {
+                    tablero.draw(spriteAlfilB);
+                    spriteAlfilB.setPosition(sf::Vector2f(distancia * i, distancia * j));
+                }
+                if ((i == 0 && j == 0) || (i == 7 && j == 0))
+                {
+                    tablero.draw(spriteTorreN);
+                    spriteTorreN.setPosition(sf::Vector2f(distancia * i, distancia * j));
+                }
+                if ((i == 0 && j == 7) || (i == 7 && j == 7))
+                {
+                    tablero.draw(spriteTorreB);
+                    spriteTorreB.setPosition(sf::Vector2f(distancia * i, distancia * j));
                 }
             }
         }
